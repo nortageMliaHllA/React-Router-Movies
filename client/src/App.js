4a51087d6180f86d5aa5f4b67a6807ba5478edd8
AllHailMegatron <ahmadrthomp@gmail.com>
2019-07-09 21:38:21 -0400

    Completed part II, now I will attempt stretch goals

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
           <Route exact path="/" component={MovieList} />
-          <Route path="movies/:id" component={Movie} />
+          <Route path="/movies/:id" component={Movie} />
         </div>
       </div>
     );
